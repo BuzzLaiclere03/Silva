22752d6962129ff1690d5146691005e423783924
Samuel Hamelin <samuelh1103@hotmail.com>
2023-05-05 10:38:14 -0400

    FInished implementing JSON for TouchScren and Leds

--- First_Prototype/Touch_Screen.py
+++ First_Prototype/Touch_Screen.py
@@ -2,7 +2,8 @@ import pathlib
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.bottomnavigation import MDBottomNavigation, MDBottomNavigationItem
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 import os
 import json
 from kivy.clock import Clock
@@ -26,7 +27,6 @@ class Menu(MDBottomNavigation):
         
         super(Menu, self).__init__(**kwargs)
         Debug.Start("Menu -> __init__")
-        Clock.schedule_interval(self.JSONupdate, 0.5)
         Debug.Log("self.Media")
         self.Media = MediaMenu()
         Debug.Log("adding Media")
@@ -43,7 +43,37 @@ class Menu(MDBottomNavigation):
         self.Settings = SettingsMenu()
         Debug.Log("Adding Settings")
         self.add_widget(self.Settings)
+        Debug.Log("self.QuitButton")
+        self.QuitButton = Quit()
+        Debug.Log("Adding QuitButton")
+        self.add_widget(self.QuitButton)
+        self.updateInfoFromJSON()
+        Clock.schedule_interval(self.JSONupdate, 0.5)
         Debug.End()
+
+    def updateInfoFromJSON(self):
+
+        # Open the file for reading
+        with open('Data_TS_W.json', 'r') as f:
+            data = json.load(f)
+
+        self.Media.Layout.Volume.Slider.value = data['Music_Volume']
+        self.Settings.Layout.Zip.text = data['Zip_Code'] 
+        self.Settings.Layout.Country.text = data['Country_Code']
+        self.Settings.Layout.Units.text = data['Units']
+        self.Source.MainLayout.Selected = data['Source'] 
+        self.Leds.Layout.Setting.Layout.Color.icon_color = data['SelectedColor']
+        self.Leds.Layout.Selection.Color1.icon_color = data['ColorPreset1']
+        self.Leds.Layout.Selection.Color2.icon_color = data['ColorPreset2']
+        self.Leds.Layout.Selection.Color3.icon_color = data['ColorPreset3']
+        self.Leds.Layout.Selection.Color4.icon_color = data['ColorPreset4']
+
+        # Open the file for writing
+        with open('Data_TS_W.json', 'w') as f:
+            json.dump(data, f)
+
+        # Close the file
+        f.close()
 
     def JSONupdate(self, dt):
 
@@ -61,6 +91,15 @@ class Menu(MDBottomNavigation):
         data['Units'] = self.Settings.Layout.Units.text
         data['Source'] = self.Source.MainLayout.Selected
         data['NewSource'] = self.Source.MainLayout.NewSource
+        if self.Leds.Layout.Setting.Layout.Color.icon == "lightbulb":
+            data['LedsOn'] = 1
+        elif self.Leds.Layout.Setting.Layout.Color.icon == "lightbulb-outline":
+            data['LedsOn'] = 0
+        data['SelectedColor'] = self.Leds.Layout.Setting.Layout.Color.icon_color
+        data['ColorPreset1'] = self.Leds.Layout.Selection.Color1.icon_color
+        data['ColorPreset2'] = self.Leds.Layout.Selection.Color2.icon_color
+        data['ColorPreset3'] = self.Leds.Layout.Selection.Color3.icon_color
+        data['ColorPreset4'] = self.Leds.Layout.Selection.Color4.icon_color
 
         # Open the file for writing
         with open('Data_TS_W.json', 'w') as f:
@@ -68,6 +107,40 @@ class Menu(MDBottomNavigation):
 
         # Close the file
         f.close()
+
+class Quit(MDBottomNavigationItem):
+
+    def __init__(self, **kwargs):
+        
+        super(Quit, self).__init__(**kwargs)
+        self.name = "Quit"
+        self.text = "Quit"
+        self.icon = "exit-to-app"
+        self.on_tab_press = self.show_alert_dialog
+
+    def show_alert_dialog(self):
+        #if not self.dialog:
+        self.dialog = MDDialog(
+            text="Do you really want to quit?",
+            buttons=[
+                MDFlatButton(
+                    text="CANCEL",
+                    theme_text_color="Custom",
+                    text_color=self.theme_cls.primary_color,
+                    on_press=self.closeDialog
+                ),
+                MDFlatButton(
+                    text="QUIT",
+                    theme_text_color="Custom",
+                    text_color=self.theme_cls.primary_color,
+                    on_press=lambda x: MDApp.get_running_app().stop()
+                ),
+            ],
+        )
+        self.dialog.open()
+
+    def closeDialog(self, xd):
+        self.dialog.dismiss()
 
 
 class Example(MDApp):
