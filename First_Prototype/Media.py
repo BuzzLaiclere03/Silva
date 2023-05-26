4f59133bf5b6c3dfaacf3aad35de472adcaaa24a
BuzzLaiclere03 <samuelh1103@hotmail.com>
2023-05-26 14:02:03 -0400

    wtf

--- First_Prototype/Media.py
+++ First_Prototype/Media.py
@@ -282,11 +282,11 @@ class MediaPlayButton(MDIconButton):
             if self.icon == "pause":
                 Debug.Log("Was pause, is now play")
                 self.icon = "play"
-                self.parent.parent.pause()
+                self.parent.parent.play()
             elif self.icon == "play":
                 Debug.Log("Was play, is now pause")
                 self.icon = "pause"
-                self.parent.parent.play()
+                self.parent.parent.pause()
             Debug.End()
 
 class MediaNextButton(MDIconButton):
