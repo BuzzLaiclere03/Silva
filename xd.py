46bcf608aebc1672cc3cde4d99588f59277941f4
BuzzLaiclere03 <samuelh1103@hotmail.com>
2023-06-02 18:56:58 -0400

    IT WOOOORKS

--- xd.py
+++ xd.py
@@ -1,32 +1,63 @@
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from datetime import datetime, timedelta
+from __future__ import print_function
 
-# Set the OAuth 2.0 scopes required for calendar access
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-# Authenticate and authorize the user using OAuth 2.0
-flow = InstalledAppFlow.from_client_secrets_file('~/.credentials/credentials.json', SCOPES)
-credentials = flow.run_local_server(port=0)
-service = build('calendar', 'v3', credentials=credentials)
 
-# Get the current date and time
-now = datetime.utcnow().isoformat() + 'Z'
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '/home/buzzlaiclere/.credentials/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
-# Calculate the end of the day
-end_of_day = (datetime.utcnow() + timedelta(days=1)).replace(hour=0, minute=0, second=0).isoformat() + 'Z'
+    try:
+        service = build('calendar', 'v3', credentials=creds)
 
-# Retrieve events from now until the end of the day
-events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=end_of_day,
-                                      maxResults=3, singleEvents=True, orderBy='startTime').execute()
-events = events_result.get('items', [])
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
 
-# Print the details of the upcoming events
-for event in events:
-    start_time = event['start'].get('dateTime', event['start'].get('date'))
-    end_time = event['end'].get('dateTime', event['end'].get('date'))
-    summary = event['summary']
-    print(f"Event: {summary}")
-    print(f"Start Time: {start_time}")
-    print(f"End Time: {end_time}")
-    print("")
+        if not events:
+            print('No upcoming events found.')
+            return
+
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+if __name__ == '__main__':
+    main()
