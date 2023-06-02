5cc56f21cb45b7236c77531533ece707ac0a8a83
Samuel Hamelin <samuelh1103@hotmail.com>
2023-06-02 19:48:16 -0400

    ahh

--- xd.py
+++ xd.py
@@ -1,63 +1,6 @@
-from __future__ import print_function
+data = [{'kind': 'calendar#event', 'etag': '"3371494158588000"', 'id': '013q8k427829mjf3b3a7hps9v4', 'status': 'confirmed', 'htmlLink': 'https://www.google.com/calendar/event?eid=MDEzcThrNDI3ODI5bWpmM2IzYTdocHM5djQgcmFuZG9tZGVtYXJkZUBt', 'created': '2023-06-02T23:04:39.000Z', 'updated': '2023-06-02T23:04:39.294Z', 'summary': 'sus', 'creator': {'email': 'randomdemarde@gmail.com', 'self': True}, 'organizer': {'email': 'randomdemarde@gmail.com', 'self': True}, 'start': {'dateTime': '2023-06-02T20:00:00-04:00', 'timeZone': 'America/Toronto'}, 'end': {'dateTime': '2023-06-02T21:00:00-04:00', 'timeZone': 'America/Toronto'}, 'iCalUID': '013q8k427829mjf3b3a7hps9v4@google.com', 'sequence': 0, 'reminders': {'useDefault': True}, 'eventType': 'default'}, {'kind': 'calendar#event', 'etag': '"3371494166882000"', 'id': '68t09amg708m8ms3e56h1537nv', 'status': 'confirmed', 'htmlLink': 'https://www.google.com/calendar/event?eid=Njh0MDlhbWc3MDhtOG1zM2U1NmgxNTM3bnYgcmFuZG9tZGVtYXJkZUBt', 'created': '2023-06-02T23:04:43.000Z', 'updated': '2023-06-02T23:04:43.441Z', 'summary': 'sy', 'creator': {'email': 'randomdemarde@gmail.com', 'self': True}, 'organizer': {'email': 'randomdemarde@gmail.com', 'self': True}, 'start': {'dateTime': '2023-06-02T21:00:00-04:00', 'timeZone': 'America/Toronto'}, 'end': {'dateTime': '2023-06-02T22:00:00-04:00', 'timeZone': 'America/Toronto'}, 'iCalUID': '68t09amg708m8ms3e56h1537nv@google.com', 'sequence': 0, 'reminders': {'useDefault': True}, 'eventType': 'default'}, {'kind': 'calendar#event', 'etag': '"3371494174204000"', 'id': '73qphm065h9utvqb31g4nggj51', 'status': 'confirmed', 'htmlLink': 'https://www.google.com/calendar/event?eid=NzNxcGhtMDY1aDl1dHZxYjMxZzRuZ2dqNTEgcmFuZG9tZGVtYXJkZUBt', 'created': '2023-06-02T23:04:47.000Z', 'updated': '2023-06-02T23:04:47.102Z', 'summary': 'baka', 'creator': {'email': 'randomdemarde@gmail.com', 'self': True}, 'organizer': {'email': 'randomdemarde@gmail.com', 'self': True}, 'start': {'dateTime': '2023-06-02T22:00:00-04:00', 'timeZone': 'America/Toronto'}, 'end': {'dateTime': '2023-06-02T23:00:00-04:00', 'timeZone': 'America/Toronto'}, 'iCalUID': '73qphm065h9utvqb31g4nggj51@google.com', 'sequence': 0, 'reminders': {'useDefault': True}, 'eventType': 'default'}]
 
-import datetime
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-
-
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                '/home/buzzlaiclere/.credentials/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    try:
-        service = build('calendar', 'v3', credentials=creds)
-
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=10, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
-
-        if not events:
-            print('No upcoming events found.')
-            return
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
-
-    except HttpError as error:
-        print('An error occurred: %s' % error)
-
-
-if __name__ == '__main__':
-    main()
+start_time = data[0]['start']['dateTime']
+print(start_time)
+start_time = data[0]['summary']
+print(start_time)
